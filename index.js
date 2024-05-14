a1b1f834f3fe5eaf350b78d1a38482cd58d70e8e
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-15 03:06:42 +0700

    change path dir product.json

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-    fs.readFile('./product.json', 'utf8', (err, data) => {
+    const filePath = path.join(__dirname, 'public', 'product.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-            res.status(500).json({message: 'error get product'});
+            res.status(500).json({ message: 'error get product' });
             return;
         }
         const products = JSON.parse(data);
-        res.status(200).json({message: 'success', product: products});
+        res.status(200).json({ message: 'success', product: products });
     });
 });
 
 app.listen(port, () => {
-    console.log(`Server  running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
