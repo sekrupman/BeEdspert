64339335ac20cf623de6af9b7eb47717db360a37
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-20 14:38:57 +0700

    edit url be

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3001;
 
 app.use(cors({
-    origin: 'https://be-edspert.vercel.app/api/products',
+    origin: 'https://be-edspert.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204
